71c873bb6b829067ba0f34b4fe55df0a8f97e114
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2022-07-26 18:03:59 -0400

    route test

--- quizz_prototype_backend/routes/api.php
+++ quizz_prototype_backend/routes/api.php
@@ -13,11 +13,6 @@ use App\Http\Controllers\webScrapperController;
 use App\Http\Controllers\FriendsController;
 use App\Http\Controllers\PaymentController;
 
-
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
