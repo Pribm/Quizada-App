3be090adeefc2f09985247c2bd1e413c7387721c
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2022-07-26 16:01:15 -0400

    payment and friendship_system implementation

--- quizz_prototype_backend/config/cors.php
+++ quizz_prototype_backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://localhost:3000'],
+    'allowed_origins' => [env('APP_VIEW_URL'),],
 
     'allowed_origins_patterns' => [],
 
