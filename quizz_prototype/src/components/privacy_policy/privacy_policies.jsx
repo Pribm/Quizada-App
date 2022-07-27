3344530a2b0c753aee5fcf89fef44c7372590594
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2022-07-26 22:09:48 -0400

    route test

---
+++ quizz_prototype/src/components/privacy_policy/privacy_policies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const privacy_policies = () => {
+    return (
+        <a href="https://www.iubenda.com/privacy-policy/51399112"
+        className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe"
+        title="Política de Privacidade ">
+            Política de Privacidade
+        </a>
+        
+    )
+}
+
+export default privacy_policies
